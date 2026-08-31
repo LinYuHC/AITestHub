22ab38c62a4d072a2c6650d7c8cf65403215a75e
linyu <1812346425@qq.com>
2026-08-31 17:03:13 +0800

    1、登录页新增：登录成功后自动跳转至首页，登录失败有提示

--- frontend/src/views/user/login.vue
+++ frontend/src/views/user/login.vue
@@ -1,29 +1,39 @@
 <script setup lang="ts">
 import {login} from "../../api/user/user.ts";
-import {ref, onMounted} from "vue";
+import {ref} from "vue";
+// 获取路由对象
+import {useRouter} from "vue-router";
 
 const username = ref('');
 const password = ref('');
+const router = useRouter();
+// 错误信息
+const errorMessage  = ref('');
 
 const loginuser = async () => {
+  // 清空错误信息
+  errorMessage.value = ''
   // 调用登录接口
   const response = await login(
       username.value,
       password.value
   )
-  // 提取Token
+  if (response.data.code === 200){
+    console.log('登录成功')
+    // 提取Token
   const token = response.data.data.access_token;
   // 保存Token到localStorage,持久化到浏览器
   localStorage.setItem('access_token', token);
+  // 跳转到首页
+  await router.push('/blog')
+  console.log('response==',response)
+  }else {
+    console.log('登录失败',response.data.message,response.data.code)
+    errorMessage.value = response.data.message || '登录失败'
+  }
 
-  console.log(response)
-  console.log('账号密码===',username.value,password.value)
-  console.log('Token===',response.data.data.access_token)
 }
 
-// onMounted(() => {
-//   loginuser()
-// })
 
 </script>
 
@@ -36,6 +46,8 @@ const loginuser = async () => {
       <input type="password" placeholder="密码" v-model="password" />
       <button type="submit">登录</button>
     </form>
+<!--    登录失败提示-->
+    <p v-if="errorMessage" class="error-message">{{ errorMessage }}</p>
   </div>
 </template>
 
