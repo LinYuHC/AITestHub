20e4f31f000ca9dc7dfcb0fb9914b55a035353a2
linyu <1812346425@qq.com>
2026-09-04 17:13:49 +0800

    1、注册全局 Pinia，添加Store状态获取

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-
+// 创建整个项目的“全局状态管理器”
+import {createPinia} from "pinia";
+// 导入用户 Store
+import { useUserStore } from './store/user'
 // 导入Router
 import router from "./router";
 
@@ -10,6 +13,16 @@ const app = createApp(App)
 
 // 注册 Vue Router,注册之后，所有页面都可以使用路由功能
 app.use(router)
+
+// 创建 Pinia
+const pinia = createPinia()
+// 注册 Pinia，Vue 项目里的组件都可以使用 Store
+app.use(pinia)
+
+// 获取用户 Store
+const userStore = useUserStore(pinia)
+// 应用启动时恢复登录状态
+await userStore.initUser()
 
 // 挂载Vue应用
 app.mount('#app')
