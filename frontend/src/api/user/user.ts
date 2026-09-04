fff25a38f46ff87d6c4011f9bfd18390d2bbd1e4
linyu <1812346425@qq.com>
2026-09-04 17:11:38 +0800

    1、增加调用退出登录接口方法：logout

--- frontend/src/api/user/user.ts
+++ frontend/src/api/user/user.ts
@@ -1,8 +1,18 @@
 import request from "../request.ts";
 
+// 登录
 export function login(username: string, password: string) {
     return request.post('/api/v1/auth/login', {
         username: username,
         password: password
     })
+}
+
+// 退出登录
+export function logout() {
+    return request.post('/api/v1/auth/logout')
+}
+
+export function getUserInfo() {
+    return request.get('/api/v1/auth/userinfo')
 }
